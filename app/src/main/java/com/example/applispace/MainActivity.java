6c6546b0737a331f7d6dab9c157e42ede35d4b3c
hugomdr <mandron@et.esiea.fr>
2019-05-31 17:34:58 +0200

    Application sur l'espace

--- app/src/main/java/com/example/applispace/MainActivity.java
+++ app/src/main/java/com/example/applispace/MainActivity.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends Activity {
     private RecyclerView recyclerView;
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        ArrayList planetes = new ArrayList<>(Arrays.asList("Mercure", "Vénus", "Terre", "Mars", "Jupiter", "Saturne", "Uranus", "Neptune"));
+        final ArrayList planetes = new ArrayList<>(Arrays.asList("Mercure", "Vénus", "Terre", "Mars", "Jupiter", "Saturne", "Uranus", "Neptune"));
         mAdapter = new MyAdapter(planetes);
         recyclerView.setAdapter(mAdapter);
     }
